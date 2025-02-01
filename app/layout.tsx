26fc76897cbb4a4ed146e703970ef7221c9326a6
Popoy-Dev <popoykua28@gmail.com>
2025-02-01 16:13:23 +0800

    Add adhense

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2334321977131410"
+     crossOrigin="anonymous"></script>
       <head>
         <meta name='google-adsense-account' content='ca-pub-2334321977131410' />
       </head>
